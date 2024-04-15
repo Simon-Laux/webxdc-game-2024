20c91cc6481888a286a0344eb2eb6bd74b4236ea
Simon Laux <mobile.info@simonlaux.de>
2024-04-15 15:01:38 +0200

    debug menu some style changes

--- src/DebugUI/PingTable.tsx
+++ src/DebugUI/PingTable.tsx
@@ -3,6 +3,7 @@ import { Name } from "../components/Name";
 import { myPeerId } from "../systems/peerId";
 import { usePeersStore } from "../systems/PeerStore";
 import { name2Color } from "../util";
+import { ChartBarIcon } from "@heroicons/react/24/outline";
 
 export function PingTable() {
   const knownPeers = usePeersStore((state) => state.knownPeers);
@@ -13,26 +14,36 @@ export function PingTable() {
         <tr>
           <td>Peer Id</td>
           <td>Last Seen At</td>
-          <td>ping</td>
+          <td style={{ display: "flex", justifyContent: "space-between" }}>
+            <span>Ping</span> <ChartBarIcon height={"1em"} />
+          </td>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td style={{ color: name2Color(myPeerId, true) }}>
-            myself ({myPeerId})
+          <td style={{ color: name2Color(myPeerId, true), maxWidth: "30vh" }}>
+            myself <br /> <small>({myPeerId})</small>
           </td>
           <td>-</td>
-          <td>-</td>
+          <td style={{ textAlign: "end" }}>-</td>
         </tr>
         {Object.keys(knownPeers).map((peerId) => {
           const peer = knownPeers[peerId];
           return (
             <tr>
-              <td style={{ color: name2Color(peer.peerId, false) }}>
-                <Name peerId={peer.peerId} /> ({peer.peerId})
+              <td
+                style={{
+                  color: name2Color(peer.peerId, false),
+                  maxWidth: "30vh",
+                }}
+              >
+                <Name peerId={peer.peerId} /> <br />{" "}
+                <small>({peer.peerId})</small>
               </td>
-              <td>{peer.last_seen}</td>
-              <td>{peer.lastPing?.ping}</td>
+              <td>
+                <small>{peer.last_seen}</small>
+              </td>
+              <td style={{ textAlign: "end" }}>{peer.lastPing?.ping}</td>
             </tr>
           );
         })}
